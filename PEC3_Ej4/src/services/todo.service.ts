27e38a3d3319fe9d3664781e607f4edf4697cdf9
aitor santaeugenia mari <asantaeugenia@uoc.edu>
2023-11-21 01:47:09 +0100

    feat(todo.services.ts): servei creat

---
+++ PEC3_Ej4/src/services/todo.service.ts
@@ -0,0 +1,57 @@
+/**
+ * @class Service
+ *
+ * Manages the data of the application.
+ */
+class TodoService {
+  todos: Todo[];
+  onTodoListChanged!: Function;
+
+  constructor() {
+    this.todos = (JSON.parse(localStorage.getItem("todos")!) || []).map(
+      (todo: {text: string; complete: boolean;}) => new Todo(todo)
+    );
+  }
+
+  public bindTodoListChanged(callback:Function) {
+    this.onTodoListChanged = callback;
+  }
+
+  _commit(todos: Todo[]) {
+    this.onTodoListChanged(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  addTodo(text: string) {
+    this.todos.push(new Todo({text, complete:false}));
+
+    this._commit(this.todos);
+  }
+
+  editTodo(id: string, updatedText: string) {
+    this.todos = this.todos.map(todo =>
+      todo.id === id
+        ? new Todo({
+            ...todo,
+            text: updatedText
+          })
+        : todo
+    );
+
+    this._commit(this.todos);
+  }
+
+  deleteTodo(_id:string) {
+    this.todos = this.todos.filter(({ id }) => id !== _id);
+
+    this._commit(this.todos);
+  }
+
+  toggleTodo(_id:string) {
+    this.todos = this.todos.map(todo =>
+      todo.id === _id ? new Todo({ ...todo, complete: !todo.complete }) : todo
+    );
+
+    this._commit(this.todos);
+  }
+}
